8a1e4afde364ca06c0b113443d844126f969de98
mnikitos2006 <121079768+mnikitos2006@users.noreply.github.com>
2023-03-27 23:29:45 +0300

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,64 +1,177 @@
-
+import {useEffect, useState} from "react";
 import './App.css';
-import {useState} from "react";
+
 
 function App() {
-    // let [name,setName]=useState('prod');
-    // let [cost,setCost]=useState('1000');
-    // let [inCart,setInCart]=useState(false);
-    // let [count,setCount]=useState(0);
-    // let [name1,setName1]=useState('Nikita');
-    // let [surname,setSurname]=useState('Mashkovsky');
-    // let [age,setAge]=useState('16');
-    let [name2,setName2]=useState("Никита");
-    let [surname1,setSurname1]=useState(false);
-    let [age1,setAge1]=useState(false);
-    let [ban,setBan]=useState(false);
-    function baan(){
-        if(name2 === false){
-            setBan(ban = true)
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    const [result, setResult] = useState("");
+    const [china, setChina] = useState('');
+
+    function handleChange(event) {
+        setChina(event.target.value);
+    }
+
+    const handleCalculate = () => {
+
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        if (!isNaN(start.getTime()) && !isNaN(end.getTime())) {
+            const diffTime = Math.abs(end - start);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+            setResult(`Разница между датами: ${diffDays} дней`);
+        } else {
+            setResult('Введён неверный формат даты')
         }
-        else{
-            setBan(ban = false)
-        }
+
+
+    };
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [valueSumma1, setValueSumma1] = useState(null)
+    const [valueSumma2, setValueSumma2] = useState(null)
+    const [valueSummaResult, setValueSummaResult] = useState(null)
+
+
+    const [valueProizved1, setValueProizved1] = useState(null)
+    const [valueProizved2, setValueProizved2] = useState(null)
+    const [valueProizvedResult, setValueProizvedResult] = useState(null)
+
+
+    const [data1, setData1] = useState('')
+    const [data2, setData2] = useState('')
+    const [dataResult, setDataResult] = useState('')
+
+
+    const [checked, setChecked] = useState(true)
+
+    const [checked1, setChecked1] = useState(true)
+    let message;
+    if (checked1){
+        message=<p>сообщение 1</p>
+    }else{
+        message=<p>сообщение 2</p>
     }
 
 
-    // function clickHandler1(){
-    //     setName('xxxx');
-    // }
-    //
-    // function clickHandler2(){
-    //     setCost('2000');
-    // }
-  return <>
-      {/*<span>{name}</span>*/}
-      {/*<span>{cost}</span>*/}
-      {/*<button onClick={()=>setName('xxxx')}>btn1</button>*/}
-      {/*<button onClick={()=>setCost('20000')}>btn2</button>*/}
-      {/*<span>{inCart ? 'в корзине':'не в корзине'}</span>*/}
-      {/*<button onClick={()=>setInCart(!inCart)}>btn3</button><br/>*/}
-      {/*<span>{count}</span>*/}
-      {/*<button onClick={()=>setCount(count+1)}>+</button><br/>*/}
-      {/*<span>{name1}</span>*/}
-      {/*<button onClick={()=>setName1('Ilya')}>smenaName</button><br/>*/}
-      {/*<span>{surname}</span>*/}
-      {/*<button onClick={()=>setName1('Ilya')}>smenaSurname</button><br/>*/}
-      {/*<span>{age}</span>*/}
-      {/*<button onClick={()=>setName1('Ilya')}>smenaAge</button><br/>*/}
+    const [checked2,setChecked2]=useState(true)
 
-      <span>{name2 ? 'Никита':'Илья'}</span>
-      <button onClick={()=>setName2(!name2)}>Смена Имени</button><br/>
+    let helloBye;
+if (checked2){
+    helloBye=<p>Привет братан!!!</p>
+}else {
+    helloBye=<p>Пока братан!!!</p>
+}
 
-      <span>{surname1 ? 'Машковский':'Фокин'}</span>
-      <button onClick={()=>setSurname1(!surname1)}>Смена Фамилии</button><br/>
 
-      <span>{age1 ? '16':'15'}</span>
-      <button onClick={()=>setAge1(!age1)}>Смена Возраста</button><br/>
-      <span>{ban ? 'ban' : 'unban'}</span>
-      <button onClick={baan}>Смена Возраста</button><br/>
-  </>
+    const [checked3,setChecked3]=useState(false)
+    let zadanie2;
+if (checked3){
+    zadanie2=' CSS '
+}else {
+    zadanie2=''
+}
 
-  }
+const [checked4,setChecked4]=useState(false)
+    let zadanie21;
+if (checked4){
+    zadanie21=' JS '
+}else {
+    zadanie21=''
+}
+
+
+const [checked5,setChecked5]=useState(false)
+    let zadanie22;
+if (checked5){
+    zadanie22=' HTML '
+}else{
+    zadanie22=''
+}
+
+    return (
+        <>
+            <input value={valueSumma1} onChange={event => setValueSumma1(event.target.value)}/>
+            <input value={valueSumma2} onChange={event => setValueSumma2(event.target.value)}/>
+            <button onClick={() => setValueSummaResult(Number(valueSumma1) + Number(valueSumma2))}>Найдёт сумму чисел
+            </button>
+            <br/>
+            <p>Сумма чисел: {valueSummaResult}</p>
+            <input value={valueProizved1} onChange={event => setValueProizved1(event.target.value)}/>
+            <input value={valueProizved2} onChange={event => setValueProizved2(event.target.value)}/>
+            <button onClick={() => setValueProizvedResult(Number(valueProizved1) * Number(valueProizved2))}>Найдёт
+                произведение чисел
+            </button>
+            <p>Произведение чисел:{valueProizvedResult} </p><br/>
+
+
+            <input value={data1} onChange={event => setData1(event.target.value)}/>
+            <input value={data2} onChange={event => setData2(event.target.value)}/>
+            <button onClick={() => setDataResult(data1 - data2)}>Узнай разницу введённых дат</button>
+            <p>Разница дат: {dataResult}</p><br/>
+
+
+            <div>
+                <p>Текущая дата: {currentDate.toLocaleDateString()}</p>
+                <button onClick={() => setCurrentDate(new Date())}>
+                    Обновить дату
+                </button>
+            </div>
+            <br/>
+            <div>
+                <input value={startDate} onChange={(e) => setStartDate(e.target.value)}/>
+                <br/>
+                <input value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
+                <br/>
+                <button onClick={handleCalculate}>Рассчитать разницу</button>
+                <br/>
+                <p>{result}</p>
+            </div>
+            <br/>
+
+
+            <div>
+                <textarea value={china} onChange={handleChange}/>
+                <p>{china}</p>
+            </div>
+            <br/>
+
+            <div>
+                <input type="checkbox" checked={true}/> отмечен
+                <input type="checkbox" checked={false}/> не отмечен
+            </div>
+            <br/>
+
+            <div>
+                <input type='checkbox' checked={checked} onChange={() => setChecked(!checked)}/>
+                <p>Состояние:{checked ? 'отмечен' : 'не отмечен'}</p>
+            </div>
+            <br/>
+
+            <div>
+                <input type='checkbox' checked={checked1} onChange={()=> setChecked1(!checked1)}/>
+                <div>{message}</div>
+            </div><br/>
+
+
+<div>
+            <input type='checkbox' checked={checked2} />
+<button  onClick={()=>setChecked2(!checked2)}>Задание 1 чекбоксы</button>
+    <p> {helloBye}</p>
+</div><br/>
+
+            <div>
+                <h1>Выбери языки, которые ты знаешь</h1>
+                <input type='checkbox' checked={checked3} onChange={()=>setChecked3(!checked3)}/> css<br/>
+                <input type='checkbox' checked={checked4} onChange={()=>setChecked4(!checked4)} /> JS<br/>
+                <input type='checkbox' checked={checked5} onChange={()=>setChecked5(!checked5)}/>HTML<br/>
+<h3>Ты знаешь : {zadanie2}{zadanie21}{zadanie22}</h3>
+
+            </div>
+
+{/*начинаю с 223 слайда*/}
+
+        </>
+    );
+}
 
 export default App;
